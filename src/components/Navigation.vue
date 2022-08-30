3737c4acff8518c6aa3c96dd54aa56042ff0922f
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2022-08-31 00:31:09 +0430

    fix

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,37 +2,6 @@
 
 <template>
   <nav class="Navigation">
-    <button
-      class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
-      type="button"
-      data-bs-toggle="offcanvas"
-      data-bs-target="#offcanvasRight"
-      aria-controls="offcanvasRight"
-    >
-      منو
-    </button>
-    <div
-      class="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96"
-      tabindex="-1"
-      id="offcanvasRight"
-      aria-labelledby="offcanvasRightLabel"
-    >
-      <div class="offcanvas-header flex items-center justify-between p-4">
-        <h5
-          class="offcanvas-title mb-0 leading-normal font-semibold"
-          id="offcanvasRightLabel"
-        >
-          Offcanvas right
-        </h5>
-        <button
-          type="button"
-          class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-        ></button>
-      </div>
-      <div class="offcanvas-body flex-grow p-4 overflow-y-auto">...</div>
-    </div>
     <div class="NavIcon">
       <!-- //////////////////////////////// -->
       <button type="button" class="BtnMessages">
@@ -69,7 +38,7 @@
                 <a
                   class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-300 hover:bg-gray-700 hover:text-white focus:text-white focus:bg-gray-700 active:bg-blue-600"
                   href="#"
-                  >تغیر اطلاعات</a
+                  >تغییر اطلاعات</a
                 >
               </li>
               <li>
