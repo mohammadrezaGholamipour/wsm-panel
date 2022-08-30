3c315250a73b27d82d9e476bea552357baafab80
محمدرضا غلامي پور معين آبادي <Gholamipour@csisint.local>
2022-08-30 11:52:41 +0430

    fix

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -25,15 +25,19 @@ TarikhOption.forEach((items) => {
     <nav aria-label="breadcrumb">
       <ol class="list-reset text-black flex items-center align-baseline">
         <li>
-          <a href="#"><font-awesome-icon icon="fa-solid fa-house-chimney" /></a>
+          <a href="#"
+            ><font-awesome-icon
+              class="text-blue-600"
+              icon="fa-solid fa-house-chimney"
+          /></a>
         </li>
         <li><span class="mx-2">/</span></li>
         <li>
-          <a href="#">زیر مجموعه</a>
+          <a href="#" class="text-yellow-600">زیر مجموعه</a>
         </li>
         <li><span class="mx-2">/</span></li>
         <li>
-          <a href="#">ارتباطات</a>
+          <a href="#" class="text-yellow-600">ارتباطات</a>
         </li>
       </ol>
     </nav>
--- src/components/NavInfo.vue
+++ src/components/NavInfo.vue
@@ -7,7 +7,7 @@
 
       <button
         type="button"
-        class="border-green-600 rounded-md NavBtn text-xl py-1 text-white bg-green-500 hover:bg-green-600"
+        class="border-green-600 rounded-md NavBtn text-xl py-1 text-white bg-green-600"
       >
         <font-awesome-icon icon="fa-solid fa-envelope" />
         <p class="ml-1">پیام ها</p>
@@ -16,7 +16,7 @@
       <!-- ////////////////////////////// -->
       <button
         type="button"
-        class="border-red-500 NavBtn mx-2 text-lg text-white bg-red-500 hover:bg-red-600"
+        class="border-red-500 NavBtn mx-2 text-lg text-white bg-red-600"
       >
         <font-awesome-icon icon="fa-solid fa-power-off" />
       </button>
