ce429c4e9a927ba781d1df74a2def90fe64a4dae
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2022-09-05 23:56:02 +0430

    fix

--- src/components/Header/FullHeader.vue
+++ src/components/Header/FullHeader.vue
@@ -129,81 +129,18 @@ const HandelFullOrDefaultScreen = () => {
           "
         />
       </button>
-      <!-- //////////////////////////////// -->
+      <!-- FinishFullOrDefaultScreen -->
     </div>
-    <div class="BreadCrumbAndDarkOrLightMoodBtn flex items-center">
-      <!-- DarkAndLightMoodBtn -->
-      <div class="content text-xl mx-2">
-        <font-awesome-icon icon="fa-solid fa-moon" class="text-blue-600" />
-        <input type="checkbox" id="tooglenight" class="cbx hidden shadow-sm" />
-        <label for="tooglenight" class="switch shadow-sm"></label>
-        <font-awesome-icon icon="fas fa-sun" class="text-orange-500" />
-      </div>
-      <!-- //////////////////////////////// -->
-      <BreadCrumb />
-    </div>
-    <!-- //////////////////////////////// -->
+
+    <BreadCrumb />
+
     <!-- MessagesBtn -->
     <button type="button" class="BtnMessages ml-1">
       <font-awesome-icon icon="fa-solid fa-envelope" />
       <p class="ml-2">پیام ها</p>
     </button>
-    <!-- //////////////////////////////// -->
+    <!-- FinishMessagesBtn -->
   </nav>
 </template>
 
-<style scoped>
-.night {
-  background-position: 0 0;
-}
-.content {
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  display: flex;
-}
-@media only screen and (max-width: 389px) {
-  .content {
-    display: none;
-  }
-}
-.switch {
-  transition: all 0.3s ease;
-  background: #2563eb;
-  border-radius: 100px;
-  position: relative;
-  cursor: pointer;
-  display: block;
-  height: 20px;
-  margin: 0 7px;
-  width: 44px;
-}
-.switch:after {
-  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.05);
-  border: solid 1.75px #9c9c9c;
-  transition: all 0.3s ease;
-  background: #ffffff;
-  border-radius: 100px;
-  position: absolute;
-  display: block;
-  height: 26px;
-  content: "";
-  width: 26px;
-  left: -2px;
-  top: -3px;
-}
-.switch:active:after {
-  transform: scale(1.15, 0.85);
-}
-.cbx:checked ~ label {
-  background: #f97316;
-}
-.cbx:checked ~ label:after {
-  left: 20px;
-  background: #ffffff;
-}
-
-.hidden {
-  display: none;
-}
-</style>
+<style></style>
