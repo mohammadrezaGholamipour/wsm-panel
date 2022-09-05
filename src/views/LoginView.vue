42e2053f0df033bf880fbfccd66437223967ffde
محمدرضا غلامي پور معين آبادي <Gholamipour@csisint.local>
2022-09-05 12:39:45 +0430

    fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,7 @@
 <template>
   <div class="LoginPage">
     <div class="LoginForm">
+      <img src="../assets/image/Logo.png" alt="مرکز خدمات حوزه های علمیه" />
       <form>
         <div class="LoginParentInput">
           <input
@@ -18,17 +19,9 @@
           />
         </div>
         <div class="LoginParentBtn">
-          <button
-            type="button"
-            class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            وارد شدن
-          </button>
+          <button type="button" class="LoginBtn">وارد شدن</button>
 
-          <a
-            href="#!"
-            class="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out"
-          >
+          <a href="#!" class="LoginForgetInfo">
             رمز یا نام کاربری خود را فراموش کرده اید؟</a
           >
         </div>
