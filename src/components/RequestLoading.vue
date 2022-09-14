82e6846c635f1a4bb48a3d3816a0a6383c66378b
محمدرضا غلامي پور معين آبادي <Gholamipour@csisint.local>
2022-09-14 14:44:36 +0430

    fix

---
+++ src/components/RequestLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="RequestLoading"></div>
+</template>
+
+<style scoped>
+.RequestLoading {
+  height: 3000px !important;
+  width: 117vw !important;
+  background: #666b70;
+  position: fixed;
+  z-index: 999;
+  opacity: 0.8;
+  padding: 0;
+  margin: 0;
+}
+</style>
--- src/components/ServiceForm.vue
+++ src/components/ServiceForm.vue
@@ -4,6 +4,7 @@ import { notify } from "@kyvg/vue3-notification";
 import { watch } from "@vue/runtime-core";
 import { reactive } from "@vue/reactivity";
 import { useRoute } from "vue-router";
+import RequestLoading from "./RequestLoading.vue";
 // ////////////////////////
 const Route = useRoute();
 const state = reactive({
@@ -12,7 +13,7 @@ const state = reactive({
   Name: "",
   Input: "",
 });
-// ////////////////////////
+// HandelRequestForWebService
 const GetWebService = (WebService) => {
   state.Loading = true;
   ServiceFormApi.Form(WebService)
@@ -25,11 +26,11 @@ const GetWebService = (WebService) => {
           title: "اطلاعات با موفقیت دریافت شد",
           ignoreDuplicates: false,
         });
-      }, 2000);
+      }, 3000);
     })
     .catch((error) => {
-      console.log(error.message);
       state.Loading = true;
+      console.log(error.message);
       notify({
         type: "error",
         title: "اطلاعات دریافت نشد لطفا مجدد امتحان کنید",
@@ -39,7 +40,7 @@ const GetWebService = (WebService) => {
     .finally(() => {
       setTimeout(() => {
         state.Loading = false;
-      }, 2000);
+      }, 3000);
     });
 };
 watch(Route, () => {
@@ -93,95 +94,46 @@ const HandelService = () => {
 };
 </script>
 <template>
-  <div class="w-full h-full">
+  <div class="ParentService">
+
+      <RequestLoading v-show="state.Loading" />
+    
     <notifications position="top center" class="mt-2" width="350" />
-    <div
-      v-if="state.Loading"
-      class="flex w-full h-full justify-center items-center space-x-2"
-    >
-      <div
-        class="spinner-grow inline-block w-8 h-8 bg-current ml-1 rounded-full opacity-0 text-purple-500"
-        role="status"
+    <img
+      src="../assets/image/WebService.png"
+      style="width: 367px; margin: 0"
+      alt="WebService"
+    />
+
+    <div class="flex justify-center flex-col items-center">
+      <input
+        class="InputService"
+        placeholder="عنوان"
+        v-model.trim="state.Name"
+        type="text"
+      />
+      <transition
+        enter-active-class="duration-500 ease-out"
+        leave-active-class="duration-200 ease-out"
+        enter-from-class="transform opacity-0"
+        leave-to-class="transform opacity-0"
+        leave-from-class="opacity-100"
+        enter-to-class="opacity-100"
       >
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div
-        class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-green-500"
-        role="status"
-      >
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div
-        class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-red-500"
-        role="status"
-      >
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div
-        class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-yellow-500"
-        role="status"
-      >
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div
-        class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-300"
-        role="status"
-      >
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div
-        class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-gray-300"
-        role="status"
-      >
-        <span class="visually-hidden">Loading...</span>
-      </div>
-    </div>
-    <transition
-      enter-active-class="duration-500 ease-out"
-      leave-active-class="duration-200 ease-out"
-      enter-from-class="transform opacity-0"
-      leave-to-class="transform opacity-0"
-      leave-from-class="opacity-100"
-      enter-to-class="opacity-100"
-    >
-      <div v-show="!state.Loading" class="ParentService">
-        <img
-          src="../assets/image/WebService.png"
-          style="width: 367px; margin: 0"
-          alt="WebService"
+        <input
+          v-show="Route.meta.isone > 0"
+          class="InputService"
+          placeholder="ورودی"
+          v-model.trim="state.Input"
+          type="text"
         />
+      </transition>
 
-        <div class="flex justify-center flex-col items-center">
-          <input
-            class="InputService"
-            placeholder="عنوان"
-            v-model.trim="state.Name"
-            type="text"
-          />
-          <transition
-            enter-active-class="duration-500 ease-out"
-            leave-active-class="duration-200 ease-out"
-            enter-from-class="transform opacity-0"
-            leave-to-class="transform opacity-0"
-            leave-from-class="opacity-100"
-            enter-to-class="opacity-100"
-          >
-            <input
-              v-show="Route.meta.isone > 0"
-              class="InputService"
-              placeholder="ورودی"
-              v-model.trim="state.Input"
-              type="text"
-            />
-          </transition>
-
-          <button class="BtnService" @click="HandelService">
-            وب سرویس
-            <font-awesome-icon icon="fa-solid fa-sliders" class="mr-2" />
-          </button>
-        </div>
-      </div>
-    </transition>
+      <button @click="HandelService" class="BtnService">
+        وب سرویس
+        <font-awesome-icon icon="fa-solid fa-sliders" class="mr-2" />
+      </button>
+    </div>
   </div>
 </template>
 <style>
