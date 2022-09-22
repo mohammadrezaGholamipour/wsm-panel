73f94d2c17ef8b79f8cabc19d138dd50312d0ef3
محمدرضا غلامي پور معين آبادي <Gholamipour@csisint.local>
2022-09-22 15:08:20 +0330

    fix

--- src/components/LogModal.vue
+++ src/components/LogModal.vue
@@ -1,31 +1,24 @@
 <script setup></script>
 <template>
   <div
-    class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
     aria-labelledby="exampleModalCenteredScrollable"
     id="exampleModalCenteredScrollable"
+    class="LogModalParent hidden fade modal"
     aria-modal="true"
     role="dialog"
-    tabindex="-1"
   >
-    <div
-      class="modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-9/12 pointer-events-none"
-    >
-      <div
-        class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current"
-      >
-        <div
-          class="modal-header flex flex-shrink-0 items-center justify-end p-4 border-b border-gray-200 rounded-t-md"
-        >
+    <div class="LogModalDivOne modal-dialog">
+      <div class="LogModalDivTow">
+        <div class="LogModalDivThree">
           <p class="text-3xl text-gray-800">نمایش لاگ ها</p>
         </div>
-        <div class="modal-body relative p-4">fgdgf</div>
+        <div class="LogModalBody">df</div>
         <div
-          class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-center p-4 border-t border-gray-200 rounded-b-md"
+          class="LogModalFooter"
         >
           <button
             type="button"
-            class="inline-block px-6 py-2.5 bg-purple-600 text-white leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+            class="BtnCloseLogModal"
             data-bs-dismiss="modal"
           >
             بازگشت
